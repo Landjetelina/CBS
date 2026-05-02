348bd39e1613b00e6f20d3158e047a8bfd8f1c10
Patrik Landeka <patrik.landeka@fer.hr>
2026-05-03 01:45:38 +0200

    removed 1 files

---
+++ grid2.py
@@ -0,0 +1,43 @@
+from functools import total_ordering
+
+class Grid:
+    def __init__(self, row, column, forbidden_nodes=None, weight=1):
+        self.row = row  # number of grid rows
+        self.column = column  # number of grid columns
+        self.forbidden_nodes:list['tuple'] = sorted(forbidden_nodes)  # represents an obstacle
+
+        self.flat_tree_list = [[weight if (i, j) not in self.forbidden_nodes
+                                else None for i in range(self.column) ] for j in range(self.row)]
+    def set_weight(self, i, j, cost):
+        self.flat_tree_list[i][j] = cost
+
+    def get_neighbours(self, i, j):
+        if not self.flat_tree_list[i][j]:
+            return None
+        if i > 0 and self.flat_tree_list[i-1][j]:
+            yield (i-1,j)
+        if j > 0 and self.flat_tree_list[i][j-1]:
+            yield (i,j-1)
+        yield (i,j)
+        if j+1 < self.column and self.flat_tree_list[i][j+1]:
+            yield (i,j+1)
+        if i+1 < self.row and self.flat_tree_list[i+1][j]:
+            yield (i+1,j)
+
+    def __str__(self):
+        string = ''
+        for i in range(self.row):
+            for j in range(self.column):
+                if not self.flat_tree_list[i][j]:
+                    string += f'{'---':^5}  '
+                else:
+                    string += f'{f"{i},{j}":^5}  '
+            string += '\n'
+        return string
+
+    # Grid 3x3 with forbidden (1,1)
+        # +y --->
+    # +x  0,0  1,0  2,0
+    #  |  0,1  ---  2,1
+    #  |  0,2  1,2  2,2
+    #  V
--- main2.py
+++ main2.py
@@ -1,6 +1,6 @@
 import lowLevel
-from lowLevel import *
-from grid import *
+from lowLevel2 import *
+from grid2 import *
 import time
 
 def create_forbidden_nodes(row_start=None, row_end=None, column_start=None, column_end=None):
@@ -12,18 +12,18 @@ def create_forbidden_nodes(row_start=None, row_end=None, column_start=None, colu
 
 if __name__ == '__main__':
     start_time = time.time()
-    N = 70
+    N = 30
     forbidden_nodes = []
     # for i in range(N):
     #     if i % 4 == 1:
-    #         forbidden_nodes.extend(create_forbidden_nodes(i,i,0,N-2,))
+    #         forbidden_nodes.extend(create_forbidden_nodes(0,N-2,i,i,))
     #     if i % 4 == 3:
-    #         forbidden_nodes.extend(create_forbidden_nodes(i,i,1,N-1))
+    #         forbidden_nodes.extend(create_forbidden_nodes(1,N-1,i,i))
 
     grid = Grid(N, N, forbidden_nodes)
     print(grid)
 
-    lowLevel = LowLevel(grid, Node(0, 0), Node(69,69))
+    lowLevel = LowLevel(grid, (0, 0), (29,29))
     solution = lowLevel.return_path()
     print(solution)
     end_time = time.time()
