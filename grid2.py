a210a1b8c713a9a5398fd13c937de618a36eedb1
Patrik Landeka <patrik.landeka@fer.hr>
2026-05-03 14:54:38 +0200

    low-level bez povratka u iste tocke s try-except u a_star

--- grid2.py
+++ grid2.py
@@ -1,14 +1,26 @@
 import select
+from pywin.idle import PyParse
 from scipy.sparse import coo
 
-
-class Pair:
+class Node:
     def __init__(self, row, column):
         self.row = row
         self.col = column
     def __eq__(self, other):
-        return isinstance(other, Pair)
+        if isinstance(other, Node):
+            return self.row == other.row and self.col == other.col
+        return False
     def __lt__(self, other):
+        if isinstance(other, Node):
+            if self.row > other.row:
+                return True
+            if self.row == other.row and self.col > other.col:
+                return True
+            return False
+        return False
+    def __str__(self):
+        return f'{self.row},{self.col}'
+
 
 
 
@@ -16,25 +28,26 @@ class Grid:
     def __init__(self, row, column, forbidden_nodes=None, weight=1):
         self.row = row  # number of grid rows
         self.column = column  # number of grid columns
-        self.forbidden_nodes:list['tuple'] = sorted(forbidden_nodes)  # represents an obstacle
+        self.forbidden_nodes:list[Node] = sorted(forbidden_nodes)  # represents an obstacle
 
         self.flat_tree_list = [[weight if (i, j) not in self.forbidden_nodes
                                 else None for i in range(self.column) ] for j in range(self.row)]
     def set_weight(self, i, j, cost):
         self.flat_tree_list[i][j] = cost
 
-    def get_neighbours(self, i, j):
+    def get_neighbours(self, node: Node):
+        i, j = node.row, node.col
         if not self.flat_tree_list[i][j]:
             return None
         if i > 0 and self.flat_tree_list[i-1][j]:
-            yield (i-1,j)
+            yield Node(i - 1, j)
         if j > 0 and self.flat_tree_list[i][j-1]:
-            yield (i,j-1)
-        yield (i,j)
+            yield Node(i, j - 1)
+        yield Node(i, j)
         if j+1 < self.column and self.flat_tree_list[i][j+1]:
-            yield (i,j+1)
+            yield Node(i, j + 1)
         if i+1 < self.row and self.flat_tree_list[i+1][j]:
-            yield (i+1,j)
+            yield Node(i + 1, j)
 
     def __str__(self):
         string = ''
--- lowLevel2.py
+++ lowLevel2.py
@@ -1,46 +1,54 @@
 from copy import deepcopy
 import heapq
 import math
+
+import sys
+
 from grid2 import *
 from sortedcontainers import SortedList
+
 
 class LowLevel:
     def __init__(self, grid: Grid, start_point, end_point):
         self.grid = grid
-        self.start_point: tuple[int, int] = start_point
-        self.end_point: tuple[int, int] = end_point
+        self.start_point: Node = start_point
+        self.end_point: Node = end_point
 
         self._nodes_expanded = 0
         # self._visited_nodes = {self.start_point: 0}
 
-    def heuristic_fn(self, node: tuple[int, int]):  # Manhattan distance
-        value = abs(self.end_point[0] - node[0]) + abs(self.end_point[1] - node[1])
+    def heuristic_fn(self, node: Node):  # Manhattan distance
+        value = abs(self.end_point.row - node.row) + abs(self.end_point.col - node.col)
         # if node in self._visited_nodes:
         #     value *= 10**self._visited_nodes[node]
         return value
 
     def a_star(self):
         # [0] is total cost + heuristic, [1] is Node, [2] is total cost, [3] is list that contains parent node
-        curr_node: list[tuple[int,int] | int | None] = [self.heuristic_fn(self.start_point), self.start_point, 0, None]
+        curr_node: list[Node | int | None] = [self.heuristic_fn(self.start_point), self.start_point, 0, None]
         open_nodes, closed_nodes = [], SortedList()
         while True:
             self._nodes_expanded += 1
-            if curr_node[1] in self._visited_nodes:
-                self._visited_nodes[curr_node[1]] += 1
-            else:
-                self._visited_nodes[curr_node[1]] = 1
-
-            neighbours = self.grid.get_neighbours(*curr_node[1])
+            # if curr_node[1] in self._visited_nodes:
+            #     self._visited_nodes[curr_node[1]] += 1
+            # else:
+            #     self._visited_nodes[curr_node[1]] = 1
+            closed_nodes.add(curr_node[1])
+            neighbours = self.grid.get_neighbours(curr_node[1])
             new_nodes = []
             for neighbour_node in neighbours:
-                cost = self.grid.flat_tree_list[neighbour_node[0]][neighbour_node[1]]
-                if neighbour_node[1] in self._visited_nodes and self._visited_nodes[neighbour_node[1]] > 1:
+                if neighbour_node in closed_nodes:
                     continue
+                cost = self.grid.flat_tree_list[neighbour_node.row][neighbour_node.col]
                 heapq.heappush(new_nodes, [self.heuristic_fn(neighbour_node) + curr_node[2] + cost,
                                 neighbour_node, curr_node[2] + cost, curr_node])
             for node in new_nodes:
                 heapq.heappush(open_nodes, node)
-            next_node: list[tuple[int, int] | int | None] = heapq.heappop(open_nodes)
+            try:
+                next_node: list[Node | int | None] = heapq.heappop(open_nodes)
+            except Exception:
+                print('Nema rješenja!')
+                sys.exit(1)
             if next_node[1] == self.end_point:
                 return next_node
             curr_node = next_node
